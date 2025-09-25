583e860dbe5566c8d81928cb68eb154cf78f7ee4
alinebm <alinebarreto.ds@gmail.com>
2025-09-25 16:18:20 -0300

    Alteração app.py

--- app.py
+++ app.py
@@ -5,4 +5,7 @@ import streamlit as st
 def load_data ():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
-st.dataframe (df)
+if __name__ =='__main__':
+    df = load_data()
+    
+    st.dataframe (df)
