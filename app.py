15ad7b5cc2874b2e6e57897eb09bf0d4def7a1c9
alinebm <alinebarreto.ds@gmail.com>
2025-11-06 10:35:03 -0300

    Adicionado sessão de respostas

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import streamlit as st
 from src.extraction import load_data
+import src.answer as asw
 
 st.set_page_config(layout='wide')
 
-def main():
-    df_raw = load_data()
+def creat_aswer_section(df):
+    st.title("Main questions answer")
+    st.header("First round")
 
-    st.dataframe (df_raw)
+    st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
+    st.subheader("How many bikes are being sold are bikes from unique owner?")
+    st.subheader("Are high kilometers bikes more expensive than bikes with lower kilometer?")
+    return None
+
+def main():
+    df = load_data()
+
+    creat_aswer_section(df)
+
+    st.dataframe (df)
 
 if __name__ =='__main__':
     main()   
